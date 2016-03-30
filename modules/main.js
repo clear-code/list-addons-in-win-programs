5d6284e136f621e486d298a46aad879ffe2a6470
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-03-30 15:43:01 +0900

    Add sample code to list existing installed applications

--- modules/main.js
+++ modules/main.js
@@ -3,3 +3,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
+
+var registry = require('registry').registry;
+var basePath = 'HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall';
+
+var installed = registry.getChildren(basePath);
+
+console.log(installed.join('\n'));
