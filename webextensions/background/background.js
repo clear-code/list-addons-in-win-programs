3b2ed1aa1473c1f935aa9a7ca9b52d17d43ddee7
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2020-12-11 10:09:32 +0900

    Start to implement addon-side codes

--- webextensions/background/background.js
+++ webextensions/background/background.js
@@ -5,3 +5,49 @@
 */
 'use strict';
 
+const mAddons = new Map();
+
+browser.management.getAll().then(async addons => {
+  addons.forEach(addToRegistry);
+  const ids = await getRegisteredAddonIds();
+  for (const id of ids) {
+    if (!mAddons.has(id))
+      removeFromRegistry(id);
+  }
+});
+
+browser.management.onInstalled.addListener(addon => {
+  console.log('onInstalled: ', addon);
+  addToRegistry(addon);
+});
+
+// This won't be called when the addon is uninstalled via about:addons. Why?
+browser.management.onUninstalled.addListener(addon => {
+  console.log('onUninstalled: ', addon);
+  removeFromRegistry(addon.id);
+});
+
+// This never been called...
+browser.management.onDisabled.addListener(addon => {
+  console.log('onDisabled: ', addon);
+  if (addon.id != browser.runtime.id)
+    return;
+
+  for (const id of mAddons.keys()) {
+    removeFromRegistry(id);
+  }
+});
+
+function addToRegistry(addon) {
+  console.log('addToRegistry: ', addon);
+  mAddons.set(addon.id, addon);
+}
+
+function removeFromRegistry(id) {
+  console.log('removeFromRegistry: ', id);
+  mAddons.delete(id);
+}
+
+async function getRegisteredAddonIds() {
+  return [];
+}
